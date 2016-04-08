2bcfb9321e2c9ddd97ffa733c7d9ff9a652e959d
Timurlan Rahimberdiev <rahimberdiev@gmail.com>
2016-04-09 02:08:30 +0600

    fix

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,7 +5,7 @@ class Book < ActiveRecord::Base
 #	validates_attachment_content_type :avatar, :content_type => /\Aimage/
 
 	# Validate filename
-#	validates_attachment_file_name :avatar, :matches => [/png\Z/, /jpe?g\Z/]
+	validates_attachment_file_name :avatar, :matches => [/png\Z/, /jpe?g\Z/]
 
 	# Explicitly do not validate
 	do_not_validate_attachment_file_type :avatar
