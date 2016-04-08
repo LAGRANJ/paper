99eeb2b8ae50b270f18b0b1c329433948f54c97c
Timurlan Rahimberdiev <rahimberdiev@gmail.com>
2016-04-09 00:55:55 +0600

    file upload

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160408165844) do
+ActiveRecord::Schema.define(version: 20160408184834) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
@@ -21,6 +21,12 @@ ActiveRecord::Schema.define(version: 20160408165844) do
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
