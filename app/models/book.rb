d8b2255653ac43e0d5c2e5d009fe78dd3bc7ca77
Timurlan Rahimberdiev <rahimberdiev@gmail.com>
2016-04-09 01:59:41 +0600

    fix some validation

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,10 +2,10 @@ class Book < ActiveRecord::Base
 	has_attached_file :avatar
 
 	# Validate content type
-	validates_attachment_content_type :avatar, :content_type => /\Aimage/
+#	validates_attachment_content_type :avatar, :content_type => /\Aimage/
 
 	# Validate filename
-	validates_attachment_file_name :avatar, :matches => [/png\Z/, /jpe?g\Z/]
+#	validates_attachment_file_name :avatar, :matches => [/png\Z/, /jpe?g\Z/]
 
 	# Explicitly do not validate
 	do_not_validate_attachment_file_type :avatar
